89b1581e87f0c45f2bb8e466f690b065ef55f0b8
abir <abir4dev@gmail.com>
2025-04-19 22:28:35 +0600

    done asg task 6

--- Lab 6 - Binary Tree [Java]/Task6.java
+++ Lab 6 - Binary Tree [Java]/Task6.java
@@ -7,8 +7,23 @@ public class Task6 {
     // At times you may need to typeCast root.elem to Integer
     // You can use extra helper private static methods with extra extra params as per need
     public static Integer levelSum( BTNode root ){
-        //TO DO
-        return null; //remove this line
+        int[] sum = new int[1];
+        levelSum(root, sum, 0);
+        return sum[0];
+    }
+
+    private static void levelSum(BTNode root,int[] sum, int lvl){
+        if (root == null){
+            return;
+        }
+        if (lvl%2 == 0){
+            sum[0] -= (Integer) root.elem;
+        }else{
+            sum[0] += (Integer) root.elem;
+        }
+
+        levelSum(root.left, sum, lvl+1);
+        levelSum(root.right, sum, lvl+1);
     }
     //============================================================================
 
